74718fbb073233e03333549dbefb8db58863ae1a
sanakoy <boeiksanakoev@bk.ru>
2026-02-22 15:13:21 +0300

    upd: добавил авторизацию ко всем ручкам

--- src/operation/api/v1/views.py
+++ src/operation/api/v1/views.py
@@ -1,6 +1,8 @@
 from uuid import UUID
 from fastapi import APIRouter, Depends
 
+from src.auth.authorization import get_current_user_by_access_token
+from src.auth.schemas import UserToken
 from src.operation.schemas import CreateOperationRequest, UpdateOperationRequest
 from src.operation.service import OperationService, get_operation_service
 from src.operation.service import OperationService
@@ -13,6 +15,7 @@ operation = APIRouter()
 async def create_operation(
     data: CreateOperationRequest,
     service: OperationService = Depends(get_operation_service),
+    auth_user: UserToken = Depends(get_current_user_by_access_token),
 ):
     await service.create_operation(data)
     return {"message": "Операция успешно создана"}
@@ -23,6 +26,7 @@ async def update_operation(
     operation_id: UUID,
     data: UpdateOperationRequest,
     service: OperationService = Depends(get_operation_service),
+    auth_user: UserToken = Depends(get_current_user_by_access_token),
 ):
     await service.update_operation(operation_id, data)
     return {"message": "Операция успешно обновлена"}
@@ -32,6 +36,7 @@ async def update_operation(
 async def delete_operation(
     operation_id: UUID,
     service: OperationService = Depends(get_operation_service),
+    auth_user: UserToken = Depends(get_current_user_by_access_token),
 ):
     deleted_operation_obj = await service.delete_operation(operation_id)
     return {"message": ("Операция успешно удалена")}
--- src/plan/api/v1/views.py
+++ src/plan/api/v1/views.py
@@ -1,6 +1,9 @@
 from uuid import UUID
 from fastapi import APIRouter, Depends
 from fastapi.params import Depends
+from src.auth.authorization import get_current_user_by_access_token
+from src.auth.schemas import UserToken
+from src.auth.schemas import UserToken
 from src.plan.schemas import CreatePlanRequest, UpdatePlanRequest
 from src.plan.service import PlanService, get_plan_service
 
@@ -10,7 +13,11 @@ plan = APIRouter()
 
 @plan.post("/create", summary="Создание плана для категории")
 async def create_plan(
-    data: CreatePlanRequest, service: PlanService = Depends(get_plan_service)
+    data: CreatePlanRequest,
+    service: PlanService = Depends(get_plan_service),
+    auth_user: UserToken = Depends(
+        get_current_user_by_access_token,
+    ),
 ):
     new_plan_obj = await service.create_plan(data)
     return {"message": "План успешно создан"}
@@ -21,6 +28,7 @@ async def update_plan(
     plan_id: UUID,
     data: UpdatePlanRequest,
     service: PlanService = Depends(get_plan_service),
+    auth_user: UserToken = Depends(get_current_user_by_access_token),
 ):
     updated_plan_obj = await service.update_plan(plan_id, data)
     return {"message": "План успешно обновлен"}
@@ -30,6 +38,7 @@ async def update_plan(
 async def delete_plan(
     plan_id: UUID,
     service: PlanService = Depends(get_plan_service),
+    auth_user: UserToken = Depends(get_current_user_by_access_token),
 ):
     deleted_plan_obj = await service.delete_plan(plan_id)
     return {"message": ("План успешно удален")}
